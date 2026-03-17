ab17584c5537a202b122a520cf541bfad541371e
vyshnavi sangani <ssanganivaishnavi@gmail.com>
2026-03-17 20:28:34 +0530

    added index.html navigation page

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+
 $(document).ready(function(){
 
+    // 1️⃣ Element Selector
+    $("p").click(function(){
+        $(this).addClass("highlight");
+    });
+
+    // 2️⃣ ID Selector
     $("#btn").click(function(){
-    $(".text").hide();
+        $("#heading").hide();
     });
-    
+
+    // 3️⃣ Class Selector
+    $(".text").dblclick(function(){
+        $(this).hide();
     });
+
+    // 4️⃣ Multiple Selector
+    $("h1, p").mouseenter(function(){
+        $(this).css("background-color", "yellow");
+    });
+
+});
